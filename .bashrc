555cf23de7d520ba250f685678e6eebc556054a1
John Reeve <reevejn@rose-hulman.edu>
2020-01-17 21:04:49 -0500

    fixes to bashrc and setup_dotfiles

--- .bashrc
+++ .bashrc
@@ -101,11 +101,8 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 # ~/.bash_aliases, instead of adding them here directly.
 # See /usr/share/doc/bash-doc/examples in the bash-doc package.
 
-alias tmuz='tmux'
-
-
 if [ -f ~/.bash_aliases ]; then
-    . ~/.bash_aliases
+    source ~/.bash_aliases
 fi
 
 # enable programmable completion features (you don't need to enable
@@ -118,7 +115,3 @@ if ! shopt -oq posix; then
     . /etc/bash_completion
   fi
 fi
-
-# virtualenv and virtualenvwrapper
-export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -14,12 +14,12 @@ cd -
 
 echo -e "\n--> Moving all exiting dotfiles to *.old"
 mv ~/.bashrc ~/.bashrc.old > /dev/null
-mv ~/.zshrc ~/.zshrc.old > /dev/null
+#mv ~/.zshrc ~/.zshrc.old > /dev/null
 mv ~/.vimrc ~/.vimrc.old > /dev/null
 mv ~/.vimextras ~/.vimextras.old > /dev/null
 mv ~/.tmux.conf ~/.tmux.conf.old > /dev/null
-mv ~/.config/ranger/rc.conf ~/.config/ranger/rc.conf.old > /dev/null
-mv ~/.config/zathura/zathurarc ~/.config/zathura/zathurarc.old > /dev/null
+#mv ~/.config/ranger/rc.conf ~/.config/ranger/rc.conf.old > /dev/null
+#mv ~/.config/zathura/zathurarc ~/.config/zathura/zathurarc.old > /dev/null
 mv ~/.bash_aliases ~/.bash_aliases.old > /dev/null
 
 echo -e "\n--> Linking dotfiles to repository "
@@ -35,21 +35,21 @@ ln -s $dir/.tmux.conf ~/.tmux.conf
 # Installing  Vundle (Vim plugin manager)
 VUNDLE=~/.vim/bundle/Vundle.vim
 if [ -d "$VUNDLE" ]; then 
-	echo -e "\n --> Vundle Already Installed, Skipping..."
+	echo -e "--> Vundle Already Installed, Skipping..."
 else 
-	echo -e "\n --> Installing Vundle (vim plugin manager)\n"
+	echo -e "  --> Installing Vundle (vim plugin manager)\n"
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim | sed "s/^/ /g"
 fi
 
 # Installing TPM (TMUX plugin manager)
 TPM=~/.tmux/plugins/tpm
 if [ -d "$TPM" ]; then
-    echo -e "\n--> TPM already installed. Skiping..."
+    echo -e "--> TPM already installed. Skiping..."
 else 
-    echo -e "\n--> Installing TPM (TMUX plugin manager)\n"
+    echo -e "  --> Installing TPM (TMUX plugin manager)\n"
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm | sed "s/^/  /g"
 fi
-echo -e "\n--> Reloading Tmux"
+echo -e "--> Reloading Tmux"
 tmux source ~/.tmux.conf
 
 
