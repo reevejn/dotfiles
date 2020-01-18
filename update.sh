c1cc6ef65151df250c76d2d8c09e27286513ef3a
John Reeve <reevejn@rose-hulman.edu>
2020-01-17 20:55:54 -0500

    fixed update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
-echo -e "--> Pulling git repo"
+echo -e "  --> Pulling git repo"
 cd $dir 
 git pull
 cd -
 
-#Vim install and update plugins
-echo -e "\n--. Installing and updating vim plugins\n"
+# Vim install and update plugins
+echo -e "  --. Installing and updating vim plugins\n"
 vim +PluginINstall! +qall
 
 
-Tmux install and update plugins 
-echo -e "\n--> INstalling and updating tmux plugins \n"
-~/.tmux/plugins/tpm/scrips/install_plugins.sh
+# Tmux install and update plugins 
+echo -e "  --> Installing and updating tmux plugins \n"
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
